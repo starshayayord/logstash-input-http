bc643f07c9b361767a2b250031657792d0684752
alexander akulov <akulov@skbkontur.ru>
2015-06-15 18:09:09 +0500

    rename

--- lib/logstash/inputs/http_file.rb
+++ lib/logstash/inputs/http_file.rb
@@ -8,7 +8,7 @@ require "uri"
 
 class LogStash::Inputs::HttpFile < LogStash::Inputs::Base
   class Interrupted < StandardError; end
-  config_name "httpfile"
+  config_name "http_file"
   default :codec, "plain"
 
   # The url to listen on.
